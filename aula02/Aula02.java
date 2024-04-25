cbde81a04581a048f10258e379c9c93925beaa85
rebecaverasa <rebecaverasa@gmail.com>
2024-04-25 10:18:32 -0300

    first commit

---
+++ aula02/Aula02.java
@@ -0,0 +1,13 @@
+package aula02;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+
+        c1.cor = "azul";
+        c1.carga = 60;
+        c1.modelo = "bic";
+        c1.ponta = 0.9f;
+        c1.tampada = false;
+    }
+}
---
+++ aula02/Caneta.java
@@ -0,0 +1,21 @@
+package aula02;
+
+public class Caneta {
+    String modelo; // Atributos (o que tem)
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void rabiscar() { // Método rabiscar (o que faz)
+
+    }
+
+    void tampar() { // Método tampar (o que faz)
+
+    }
+
+    void destampar() { // Método destampar (o que faz)
+
+    }
+}
